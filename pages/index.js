f7ae5452404834cb34eef2498270da9c706dc128
Tobias Brasser <tbr142625@stud.gibb.ch>
2024-06-09 14:18:37 +0200

    project upload

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Styles from "./index.module.css"
+import Footer from '../components/footer'
+import Head from 'next/head'
+
+
+
+function IndexPage() {
+  return (
+    <>
+        <Head>
+                <title>Home</title>
+                <meta name="description" content="Beschreibung der Startseite" />
+                <link rel="icon" href="../public/logo.png" />
+        </Head>
+
+        <div className={Styles.gallery}>
+            {/* <img className={Styles.image} src="/panda.JPG" /> */}
+            <div className={Styles.textBox}>
+              <h1 className={Styles.title}>Herzlich Willkommen zu meiner Homepage</h1>
+            </div>
+        </div>
+        <Footer />
+    </>
+  ) /** Test */
+}
+
+export default IndexPage
