d9853b757f7e9e07773c14b40df54b5ba38667e9
Tobias Brasser <tbr142625@stud.gibb.ch>
2024-06-10 09:41:33 +0200

    fix all images

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
+import Footer from '../components/Footer'
 
 
 export default function AppPage({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <link rel="icon" href="/logo-color.png" />
+        <link rel="icon" href="./logo-color.png" />
       </Head>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </div>
   )
 }
