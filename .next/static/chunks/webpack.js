6744aa154782cf69ee16ea036f4712cc4694aa33
Tobias Brasser <tbr142625@stud.gibb.ch>
2024-06-10 09:36:27 +0200

    try image fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b36440542107aa3e"; }
+/******/ 		__webpack_require__.h = function() { return "7d925dbf95b3c8ac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
